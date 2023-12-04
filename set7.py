62d90fa855d5c7cda37fa6467335c77952afaa8a
Meriem Soubih <soubih.meriem@gmail.com>
2023-12-04 08:58:41 +0100

    set seven completed

---
+++ set7.py
@@ -0,0 +1,24 @@
+# Unique In Order -------------------------------------------------------------
+def unique_in_order(seq):
+   	return [e for i, e in enumerate(seq) if i == 0 or e != seq[i-1]]
+
+
+# Find the unique number ------------------------------------------------------
+def find_uniq(arr):
+    uni = arr[0]
+    if arr.count(uni) == 1:
+        return uni
+    for e in arr:
+        if e != uni:
+            return e
+        
+
+# Replace With Alphabet Position
+def alphabet_position(text):
+    alphabit = "abcdefghijklmnopqrstuvwxyz"
+    text = text.lower()
+    result = ""
+    for char in text:
+        if char.isalpha():
+            result += str(alphabit.index(char) + 1) + ' '
+    return result.strip()
