3e0dca646c6594997eba565c67acc49bd840eeb6
Meriem Soubih <soubih.meriem@gmail.com>
2023-12-24 18:28:09 +0100

    set9 update valid braces

--- set9.py
+++ set9.py
@@ -5,4 +5,16 @@ def find_missing_letter(array):
             return chr(ord(array[i]) + 1)
 
 
-# 
+# Valid Braces ---------------------------------------------
+def valid_braces(string):
+    stack = []
+    for e in string:
+        if e in '{([':
+            stack.append(e)
+        elif e in '])}':
+            if not stack:
+                return False
+            top = stack.pop()
+            if (e == ')' and top != '(') or (e == '}' and top != '{') or (e == ']' and top != '['):
+                return False
+    return not stack
