5795918e685a8759b4f469c659bd14993138c88b
Meriem Soubih <soubih.meriem@gmail.com>
2023-12-26 11:26:36 +0100

    Optimize cumulative_triangle function

--- set12.py
+++ set12.py
@@ -1,1 +1,20 @@
-# 
+# Cumulative Triangle ------------------------------------------------------
+# NOT OPTIMAL !!!!!!! TIMED OUT
+# def cumulative_triangle(n):
+#     if n == 1:
+#         return 1
+#     sum = 0
+#     elem = 0
+#     for i in range(1, n+1):
+#         for j in range (1, i+1):
+#             elem +=1
+#             if i == n:
+#                 sum += elem
+#     return sum
+# OPTIMIZED VER.
+def cumulative_triangle(n):
+    start = (n*(n-1))//2 + 1
+    end = (n*(n+1))//2
+    return n * (start + end) // 2
+
+
