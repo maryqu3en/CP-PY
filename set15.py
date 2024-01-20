7789f6b9fcf1b87e66912563856be869999099f6
Meriem Soubih <soubih.meriem@gmail.com>
2024-01-20 12:04:00 +0100

    roman decoder

--- set15.py
+++ set15.py
@@ -51,3 +51,16 @@ def solution(n):
     return roman
 
 
+# Roman Numerals Decoder
+def solution(roman: str) -> int:
+    dic = {'I':1,'IV':4,'V':5,'IX':9,'X':10,'XL':40,'L':50,'XC':90,'C':100,'CD':400,'D':500,'CM':900,'M':1000}
+    num = 0
+    i = 0
+    while i < len(roman):
+        if i < len(roman) - 1 and roman[i:i+2] in dic:
+            num += dic[roman[i:i+2]]
+            i += 2
+        else:
+            num += dic[roman[i]]
+            i += 1
+    return num
