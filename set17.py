582539c8fc3573319d91bbf327de282995a48c8c
Meriem Soubih <soubih.meriem@gmail.com>
2024-01-23 00:29:04 +0100

    finish

--- set17.py
+++ set17.py
@@ -24,3 +24,18 @@ def bitplusplus(n):
         x += 1 if "++" in input() else -1
     return x
 # print(bitplusplus(int(input())))
+
+
+# Domino piling
+def domino_piling(m, n):
+    return (m*n)//2
+# m, n = map(int, input().split())
+# print(domino_piling(m, n))
+
+
+# Petya and Strings
+def petya_and_strings(s1, s2):
+    s1 = s1.lower()
+    s2 = s2.lower()
+    return 1 if s1 > s2 else -1 if s1 < s2 else 0
+# print(petya_and_strings(input(), input()))
